1eb2530ee48e6536ade7d60dac4378129f44e5a1
Artem Krol <artemkrol47@gmail.com>
2023-11-25 12:01:08 -0600

    add page Dialogs

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/NavBar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      
+      <div>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
     </div>
   );
 }
