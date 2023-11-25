8ff9742e951765734253a166ed2f46899fc99985
Artiom Krol <60381661+darkarki83@users.noreply.github.com>
2023-11-25 14:35:40 -0600

    #3 lessons 18 20 finished (#4)
    * add page Dialogs
    
    * added routing

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/NavBar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <Router>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+          <Route path="/" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ facebook/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,56 @@
+import s from './Dialogs.module.css';
+
+export default function Dialogs (props) {
+
+    const mockDialogs = [
+        { name: "Alice", id: 1 },
+        { name: "Bob", id: 2 },
+        { name: "Charlie", id: 3 },
+        { name: "Diana", id: 4 },
+        { name: "Eve", id: 5 }
+      ];
+      
+      const mockMessages = [
+        { message: "Hi there!", id: 1 },
+        { message: "How are you?", id: 2 },
+        { message: "Let's meet tomorrow.", id: 3 },
+        { message: "Thank you!", id: 4 },
+        { message: "See you soon.", id: 5 }
+      ];
+
+  //let dialogsElements = mockDialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+  //let messagesElements = mockMessages.map(m => <Message message={m.message} />);
+  let newMessageText = props.newMessageText;
+
+  let onSendMessageClick = () => {
+    props.sendMessage();
+  };
+
+  let onNewMessageChange = (e) => {
+    let text = e.target.value;
+    props.updateNewMessageText(text);
+  };
+
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogs_items}>
+        {/* {dialogsElements} */}
+      </div>
+      <div className={s.messages}>
+        {/* {messagesElements} */}
+        <div>
+          <div>
+            <textarea
+            //   value={newMessageText}
+            //   onChange={onNewMessageChange}
+              placeholder="Enter your message"
+            ></textarea>
+          </div>
+          <div>
+            <button onClick={onSendMessageClick}>Send message</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
