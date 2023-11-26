9e51765f4364b319cdef9b565c8acda9652929fe
Artem Krol <artemkrol47@gmail.com>
2023-11-26 12:21:28 -0600

    added mapping in MyPosts

--- facebook/src/components/Profile/MyPosts/MyPosts.jsx
+++ facebook/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,20 @@ import React, { useState } from 'react';
 
 export default function MyPosts() {
 
+  const posts = [
+    { message: "Hi how are you", likes: "7", id: 1 },
+    { message: "Its my first post", likes: "28", id: 2 },
+    { message: "Beautiful day outside!", likes: "15", id: 3 },
+    { message: "Learning React is fun", likes: "22", id: 4 },
+    { message: "Just finished a great book", likes: "18", id: 5 },
+    { message: "Working on a new project", likes: "30", id: 6 },
+    { message: "Cooked a delicious meal today", likes: "25", id: 7 },
+    { message: "Went for a long walk", likes: "12", id: 8 },
+    { message: "Started learning a new language", likes: "20", id: 9 }
+  ];
+
+  let postsElements = posts.map(p => <Post message={p.message} likes={p.likes} />);
+
   const [text, setText] = useState('');
 
   const addPost = () => {
@@ -29,8 +43,7 @@ export default function MyPosts() {
         <button onClick={removeText}>Remove</button>
       </div>
       <div className='item'>
-        <Post message="Hi how are you" likes="7" />
-        <Post message="Its my first post" likes="28" />
+        {postsElements}
       </div>
     </div>
   )
